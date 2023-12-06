770b30f337e6037144855eb12c368b1a6b008d48
SaidIbenariba <saidibenariba@gmail.com>
2023-12-06 19:31:48 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import { useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const drumPads = [
+    {
+      key: "Q",
+      name: "Heater 1",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "W",
+      name: "Heater 2",
+      url: "https://www.freesoundeffects.com/preview.php?id=39805",
+    },
+    {
+      key: "E",
+      name: "Heater 3",
+      url: "https://www.freesoundeffects.com/files/mp3_89357.mp3",
+    },
+    {
+      key: "A",
+      name: "Heater 4",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "S",
+      name: "Clap",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "D",
+      name: "Open High Hat",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "Z",
+      name: "Kick n' Hat",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "X",
+      name: "Kick",
+      url: "https://www.freesoundeffects.com/files/mp3_466176.mp3",
+    },
+    {
+      key: "C",
+      name: "Closed High Hat",
+      url: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3",
+    },
+  ];
+
+  const [currentDrum, setDrum] = useState("");
+
+  useEffect(() => {
+    window.addEventListener("keydown", (event) => {
+      const upperKey = event.key.toUpperCase();
+      const foundDrum = drumPads.find((item) => item.key === upperKey);
+      play(upperKey, foundDrum.name);
+    });
+  });
+
+  function play(key, drum) {
+    const audio = document.getElementById(key);
+    setDrum(drum);
+    audio.currentTime = 0;
+    audio.play();
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="drum-machine" className="container">
+      <h1 className="h1 drum-heading">React Drum Machine</h1>
+      <h4 id="display">{currentDrum}</h4>
+      <div className="row">
+        <div className="col text-center">
+          <DrumDisplay drumPads={drumPads} play={play} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+function DrumDisplay(props) {
+  return props.drumPads.map((pad, index) => {
+    return (
+      <button
+        className="drum-pad"
+        onClick={() => props.play(pad.key, pad.name)}
+        key={index}
+        id={pad.name}
+      >
+        {pad.key}
+        <audio className="clip" id={pad.key} src={pad.url} />
+      </button>
+    );
+  });
+}
+
+export default App;
