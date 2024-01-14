01cb7c6606b4b78e028d0a4ff6864295e9a5140d
SaidIbenariba <saidibenariba@gmail.com>
2024-01-14 17:35:39 +0100

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
-import { ReactDOM } from "react";
 
 export default function App() {
   const drumPads = [
